a23079971df9d3f8b5b9c98222dcdf133aae9a75
dereal-code <prince.owusu-nkrumah@vanderbilt.edu>
2022-06-06 18:49:57 -0700

    yo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- *  *
- *   *  * Description: main - Prints out a string to stdout.
- *    *
- *     *   * Return: 0 if success.
- *      *
- *       *    */
+ *Description: main - Prints out a string to stdout
+ *comments in C source code.
+ *Please use it consistently.
+ *Return: 0 if success
+ *Description:  A column of asterisks on the left side,
+ *with beginning and ending almost-blank lines.
+ */
 
 int main(void)
 
