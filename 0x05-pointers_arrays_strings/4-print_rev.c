884477af1c01d65a1fe7b1323049c4aad9c041a4
dereal-code <100052714+dereal-code@users.noreply.github.com>
2022-06-13 18:36:38 +0000

    Update 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - prints in reverse
+ * @s: input string
+ */
+void print_rev(char *s)
+{
+	int x = strlen(s) - 1;
+	
+	while (x >= 0)
+	{
+		_putchar(s[x]);
+		x--;
+	}
+	_putchar('\n');
+}
