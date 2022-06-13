783368e63d232d04bc148cb66a670a7870ec360a
dereal-code <100052714+dereal-code@users.noreply.github.com>
2022-06-13 18:00:53 +0000

    Create 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - functions prints lowercase chars
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	const char alpha[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G',
+		'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+		'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+	int x;
+
+	for (x = 0; x < 26; x++)
+	{
+		putchar(tolower(alpha[x]));
+	}
+	for (x = 0; x < 26; x++)
+	{
+		putchar(alpha[x]);
+	}
+	putchar('\n');
+	return (0);
+}
