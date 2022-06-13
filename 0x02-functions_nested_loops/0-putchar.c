bc317dc85a389f75142cf081f0b8f987524fc40d
dereal-code <100052714+dereal-code@users.noreply.github.com>
2022-06-13 15:59:03 +0000

    Update 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+#include "main.h"
+
+/**
+ *
+ *  * main - main - Entry point
+ *
+ *   *
+ *
+ *    * desc:program that prints _putchar, followed by a new line.
+ *
+ *     * The program should return 0
+ *
+ *      * Return: Always 0 (Sucess)
+ *
+ *       */
+
+int main(void)
+
+{
+
+		putchar('_');
+
+			putchar('p');
+
+				putchar('u');
+
+					putchar('t');
+
+						putchar('c');
+
+							putchar('h');
+
+								putchar('a');
+
+									putchar('r');
+
+										putchar('\n');
+
+
+
+											return (0);
+
+}
